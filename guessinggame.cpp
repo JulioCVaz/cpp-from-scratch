b8ae5370b9c219ec1ef51b19324a7f5cc29f1e62
JulioCVaz <julio.oliveiravaz0@gmail.com>
2022-07-08 12:53:04 -0300

    feat: implements conditional statements

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -5,5 +5,23 @@ using namespace std;
 int main (){
     cout << "********************************" << endl;
     cout << "* Welcome to the guessing game *" << endl;
-    cout << "********************************" << endl; 
+    cout << "********************************" << endl;
+
+    int secret_number = 42;
+    int choice;
+
+    cout << "What's your choice?";
+    cin >> choice;
+
+    cout << "The value of your choice is: " << choice << endl;
+
+    if(choice == secret_number) {
+        cout << "Congrats! You choice a correct number!" << endl;
+    }
+    else if(choice > secret_number) {
+        cout << "Your choice is more than secret number!" << endl;
+    }
+    else {
+        cout << "Your choice is less than secret number!" << endl;
+    }
 }
