10966dc21abd67871317dabf962e491b2e66e40c
JulioCVaz <julio.oliveiravaz0@gmail.com>
2022-07-08 14:37:19 -0300

    feat: add looping statements

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -8,23 +8,34 @@ int main (){
     cout << "********************************" << endl;
 
     const int SECRET_NUMBER = 42;
-    int choice;
 
-    cout << "What's your choice?";
-    cin >> choice;
+    bool not_guessed = true;
+    int attempts = 0;
 
-    cout << "The value of your choice is: " << choice << endl;
+    while(not_guessed) {
+        attempts++;
+        int choice;
 
-    bool correct_value = choice == SECRET_NUMBER;
-    bool more_than_value = choice > SECRET_NUMBER;
+        cout << "attempt " << attempts << endl;
+        cout << "What's your choice?";
+        cin >> choice;
 
-    if(correct_value) {
-        cout << "Congrats! You choice a correct number!" << endl;
+        cout << "The value of your choice is: " << choice << endl;
+
+        bool guessed = choice == SECRET_NUMBER;
+        bool more_than_value = choice > SECRET_NUMBER;
+
+        if(guessed) {
+            cout << "Congrats! You choice an correct number!" << endl;
+            not_guessed = false;
+        }
+        else if(more_than_value) {
+            cout << "Your choice is more than secret number!" << endl;
+        }
+        else {
+            cout << "Your choice is less than secret number!" << endl;
+        }
     }
-    else if(more_than_value) {
-        cout << "Your choice is more than secret number!" << endl;
-    }
-    else {
-        cout << "Your choice is less than secret number!" << endl;
-    }
+    cout << "end game!" << endl;
+    cout << "You guessed it in " << attempts << " attempts" << endl;
 }
