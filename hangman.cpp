bf19c819618acb8efd5f4f7e7072348a04447705
Juliovaz <julio.oliveiravaz0@gmail.com>
2022-09-17 20:28:33 -0300

    feat: add map structure

--- hangman.cpp
+++ hangman.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
+#include <map>
+
 using namespace std;
 
-const string SECRETE_WORD = "WATERMELON";
+const string SECRET = "WATERMELON";
+map<char, bool> already_choice;
 
 bool check_if_choice_exists(char choice) {
-    for(char letter : SECRETE_WORD) {
+    for(char letter : SECRET) {
         if (letter == choice) {
             return true;
         }
@@ -14,14 +17,23 @@ bool check_if_choice_exists(char choice) {
 }
 
 int main() {
-    cout << SECRETE_WORD << endl;
-
     bool incorrect = true;
     bool hanged = true;
 
     while(incorrect && hanged) {
+        for (char letter : SECRET) {
+            if (already_choice[letter]) {
+                cout << letter << " ";
+            } else {
+                cout << "_ ";
+            }
+        }
+        cout << endl;
+
         char choice;
         cin >> choice;
+
+        already_choice[choice] = true;
 
         if (check_if_choice_exists(choice)) {
             cout << "Correct! you're right" << endl;
